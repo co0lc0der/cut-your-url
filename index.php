97baab0c04c1076b79e501cf183ffb3ce8cf8ed8
co0lc0der <c0der@ya.ru>
2021-10-08 06:12:51 +0500

    split header & footer

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include 'includes/header.php'; ?>
+<main class="container">
+	<div class="row mt-5">
+		<div class="col">
+			<h2 class="text-center">Необходимо <a href="register.html">зарегистрироваться</a> или <a href="login.html">войти</a> под своей учетной записью</h2>
+		</div>
+	</div>
+	<div class="row mt-5">
+		<div class="col">
+			<h2 class="text-center">Пользователей в системе: 100</h2>
+		</div>
+	</div>
+	<div class="row mt-5">
+		<div class="col">
+			<h2 class="text-center">Ссылок в системе: 200</h2>
+		</div>
+	</div>
+	<div class="row mt-5">
+		<div class="col">
+			<h2 class="text-center">Всего переходов по ссылкам: 300</h2>
+		</div>
+	</div>
+</main>
+<?php
+include 'includes/footer.php';
