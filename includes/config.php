f0c317241ce16761d2ade8337f9c518a44465889
co0lc0der <c0der@ya.ru>
2021-10-12 18:08:34 +0500

    js folder

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1); // дает команду интерпретат�
 define('SITE_NAME', "Cut your URL");
 define('HOST', "http://{$_SERVER['HTTP_HOST']}"); // http сервер
 define('DB_HOST', "127.0.0.1"); // сервер БД
-define('DB_NAME', "red"); // имя БД
+define('DB_NAME', "db_red"); // имя БД
 define('DB_USER', "root"); // логин к БД
 define('DB_PASS', "4321"); // пароль к БД
 
